5a9bc370cee6e452bd8c48a6bea83e3e0361d243
facundoosti <facundoosti@gmail.com>
2013-10-31 18:28:35 -0300

    lala

--- practica3/ejercicio_doce/app.rb
+++ practica3/ejercicio_doce/app.rb
@@ -3,13 +3,11 @@ Bundler.require
 
 helpers do
 	def form method, action, &block
-		metodo = method.scan(/\w+/).last
-		accion = action.scan(/\S+/).last
-		'<form method="post" action='+accion+'><input type="hiden" name="_method" value="'+metodo+'"/></form>'+ yield(block)
+		'<form method="post" action='+action[:action]+'>'+ yield(block)+'</form>'
 	end
 	
-	def submit name
-		'<input type="submit" value='+name+'>'
+	def input type, name
+		'<input type='+type+' name="_method" value='+name+'>'
 	end
 end
 
