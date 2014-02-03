226f020fa76ba03ed9583ce441bb8350eaa66e54
facundoosti <facundoosti@gmail.com>
2014-02-03 18:50:13 -0300

    add

--- TpFinal/test/test_helper.rb
+++ TpFinal/test/test_helper.rb
@@ -5,16 +5,12 @@ Bundler.require :default, ENV['RACK_ENV'].to_sym
 require File.expand_path(File.join('init'))
 require_relative '../app/helpers.rb'
 
-require 'minitest/autorun'
-require 'minitest/spec'
 require 'time'
 
 DatabaseCleaner.strategy = :transaction
 
 class MiniTest::Spec
-  include FactoryGirl::Syntax::Methods
-  FactoryGirl.find_definitions
-  
+
   before :each do
     DatabaseCleaner.start
   end
